e9821b7d517dc2a29ef6793f84b2b401d4717bb9
Thiccarus-13 <113143044+Thiccarus-13@users.noreply.github.com>
2025-02-21 18:21:44 -0700

    started the gameTileScript

---
+++ Assets/Scripts/GameTileScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameTile : MonoBehaviour
+{
+    private int value = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
